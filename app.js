e3695306679a02c40a3db6087cd4304f23d82c73
Digitali <59025363+m00nl1ghts@users.noreply.github.com>
2020-11-15 09:20:18 +0000

    big update

--- app.js
+++ app.js
@@ -1,99 +1,146 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+console.log(`Starting SMTB!`)
 client.once('ready', () => {
-	console.log('SMTB is ready! Shake a leg with >');
+	console.log(`SMTB is ready! Logged in as ${client.user.tag}`);
+	client.channels.cache.get('762360845577486336').send(`**Bot is online. You can turn these notifications off in the bots app.js file. <@345650591713984513>**`);
+    client.user.setPresence({ activity: { name: 'Type >help for help. | Welcome to SMT'}, status: 'dnd' })
+
 });
 
 const { prefix, token } = require('./config.json');
-//regular commands and swear filter begin here.
+//regular commands and swear filter begin here and status code.
 client.on('message', message => {
 
-			if (message.content === `${prefix}help`) {
-        	message.channel.send(`**Regular Commands**\n\n**>serverinfo** [Displays Server Information]\n**>userinfo** [Displays User Information]\n**>botinfo** [Displays Bot Information]\n**>modsinfo**[Displays SMT mods info.]\n\n*Custom Commands*\n\n**>zoe**\n**>digitali**`);
+		     if (message.content === `${prefix}help`) {
+            const helpCmd = new Discord.MessageEmbed()
+            	.setColor('RED')
+            	.setTitle('Commands')
+            	.setURL('')
+            	.setDescription(`Commands below\n>serverinfo = Shows server information.\n>userinfo = Shows user information\n>modsinfo = Shows mod information using green and red boxes.\n>botinfo = Shows bot information.\n>updatelog = Shows last bot update time`)
+            	.setTimestamp()
+
+            message.channel.send(helpCmd);
 
         } else if (message.content === `${prefix}serverinfo`) {
-        	message.channel.send(`**Server Name : ${message.guild.name}!**\n**Total Members : ${message.guild.memberCount}!**\n**Server Region : ${message.guild.region}**\n**Server Creation Date : ${message.guild.createdAt}**`);
+         const siCmd = new Discord.MessageEmbed()
+              .setColor('RED')
+              .setTitle(`${message.guild.name} Information`)
+              .setDescription(`Server Name : ${message.guild.name}!**\n**Total Members : ${message.guild.memberCount}!**\n**Server Region : ${message.guild.region}**\n**Server Creation Date : ${message.guild.createdAt}`)
+              .setTimestamp()
+
+            message.channel.send(siCmd);
+
+
 
         } else if (message.content === `${prefix}userinfo`) {
-            message.channel.send(`**User Account Username and Discriminator: ${message.author.tag}**\n**${message.author.username}'s ID: ${message.author.id}**\n**${message.author.username}'s time of account creation: ${message.author.createdAt}**\n**${message.author.username}'s Avatar URL : ${message.author.avatarURL}**\n**${message.author.username}'s **`);
+         const uiCmd = new Discord.MessageEmbed()
+        	.setColor('RED')
+        	.setTitle(`${message.author.tag} Information`)
+        	.setDescription(`**User Account Username and Discriminator: ${message.author.tag}**\n**${message.author.username}'s ID: ${message.author.id}**\n**${message.author.username}'s time of account creation: ${message.author.createdAt}**`)
+        	.setTimestamp()
+
+        	message.channel.send(uiCmd)
 
         } else if (message.content === `${prefix}modsinfo`) {
-            message.channel.send(`null`);
+        const miCmd = new Discord.MessageEmbed()
+        .setColor('RED')
+        .setTitle(`Mod Information`)
+        .setDescription(`Mods:\n\nTARDIM\nAsassins Creed Mod (ACM)\n\nTARDIM Progress: 🟩🟩🟩🟩🟩🟥\n*----------*\nACM Progress: 🟥🟥🟥🟥🟥🟥`)
+        .setTimestamp()
+
+        message.channel.send(miCmd)
 
         } else if (message.content === `${prefix}botinfo`) {
-            message.channel.send(`**SMT Bot is coded by Digitali. It will auto-mod, provide server information, etc. You can add this bot to your own server, as long as you dont claim it as your own.**`);
+               const biCmd = new Discord.MessageEmbed()
+                    .setColor('RED')
+                    .setTitle(`SMT Bot Information`)
+                    .setDescription(`SMT is currently being coded by Atomical in the language known as JavaScript and discord.js. This bots current features are :\nAuto-Mod (removing forbidden words.)\nProvides server/user/bot/mods information. (see >help).`)
+                    .setTimestamp()
 
-        } else if (message.content === `${prefix}modinfo`) {
-            message.channel.send(`Mod is currently around `)
+                    message.channel.send(biCmd)
+
+        } else if (message.content === `${prefix}updatelog`) {
+        const ulCmd = new Discord.MessageEmbed()
+               .setColor('BLUE')
+               .setTitle(`Last Bot Update`)
+               .setDescription(`Sat 11:36 GMT 31/10/2020 <:pumpkin:772061093265670214>`)
+               .setTimestamp()
+                message.channel.send(ulCmd)
+
+
 // swear filter part and regular commands end
         } else if (message.content === `Fuck`) {
-        var role = message.guild.roles.find("name", "Warning 1");
-           message.member.addRole(role);
-           message.delete(`Fuck`)
+           message.delete({ timeout: 50 })
             message.channel.send(`Please do not continue to break rules or use words that are disallowed. You can read the rules in <#734174100557266956>`);
 
         } else if (message.content === `Cunt`) {
-         var role = message.guild.roles.find("name", "Warning 1");
-           message.member.addRole(role);
-           message.delete(`Cunt`)
+           message.delete({ timeout: 50 })
             message.channel.send(`Please do not continue to break rules or use words that are disallowed. You can read the rules in <#734174100557266956>`);
 
         } else if (message.content === `Nigga`) {
-         var role = message.guild.roles.find("name", "N-Word Warning");
-           message.member.addRole(role);
-           message.delete(`Nigga`)
+           message.delete({ timeout: 50 })
             message.channel.send(`Please do not use racist words within our Discord! Do it again and you will receive a ban.`);
 
         } else if (message.content === `fuck`) {
-         var role = message.guild.roles.find("name", "Warning 1");
-           message.member.addRole(role);
-           message.delete(`fuck`)
+           message.delete({ timeout: 50 })
             message.channel.send(`Please do not continue to break rules or use words that are disallowed. You can read the rules in <#734174100557266956>`);
 
         } else if (message.content === `cunt`) {
-         var role = message.guild.roles.find("name", "Warning 1");
-           message.member.addRole(role);
-           message.delete(`cunt`)
+           message.delete({ timeout: 50 })
             message.channel.send(`Please do not continue to break rules or use words that are disallowed. You can read the rules in <#734174100557266956>`);
 
          } else if (message.content === `nigga`) {
-         var role = message.guild.roles.find("name", "N-Word Warning");
-           message.member.addRole(role);
-           message.delete(`nigga`)
+           message.delete({ timeout: 50 })
             message.channel.send(`Please do not use racist words within our Discord! Do it again and you will receive a ban.`);
 
           } else if (message.content === `emm`) {
-          message.delete(`emm`)
+          message.delete({ timeout: 50 })
           message.author.send(`No emm.`);
 
           } else if (message.content === `Emm`) {
-          message.delete(`Emm`)
+          message.delete({ timeout: 50 })
           message.author.send(`No emm.`);
 
           } else if (message.content === `EMM`) {
-           message.delete(`EMM`)
+           message.delete({ timeout: 50 })
            message.author.send(`No emm, and watch your caps.`);
 
           } else if (message.content === `eMm`) {
-           message.delete(`eMm`)
+           message.delete({ timeout: 50 })
            message.author.send(`No emm.`);
 
            } else if (message.content === `emM`) {
-           message.delete(`emM`)
+           message.delete({ timeout: 50 })
            message.author.send(`No emm.`);
 
 
 
      //swear filter ends here, custom commands start here.
 
-        } else if (message.content === `>zoe`) {
+        } else if (message.content === `${prefix}zoe`) {
             message.channel.send(`Zoe is the best lesbo. Dont @ her..`);
 
-        } else if (message.content === `>digitali`) {
-            message.channel.send(`Digitali will slaughter you if she finds out you did this command!`);
+        } else if (message.content === `${prefix}atomical`) {
+            message.channel.send(`Atomical will slaughter you if she finds out you did this command!`);
 
-     //custom commands end here. status code, dm reply code, welcome message and client login code starts here.
+} else if (message.content === `${prefix}botcode`) {
+const botcodeEmbed = new Discord.MessageEmbed()
+	.setColor('#0099ff')
+	.setTitle('Example Bot Code')
+	.setURL('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+	.setAuthor('Example Bot Code')
+	.setDescription('Code below has status stuff in it and stuff. Enjoy.\ne')
+	.setThumbnail('https://cdn.discordapp.com/attachments/762360845577486336/762751276235882506/Screenshot_2020-10-05_at_20.01.05.png')
+	.setImage('https://cdn.discordapp.com/attachments/762360845577486336/762751276235882506/Screenshot_2020-10-05_at_20.01.05.png')
+	.setTimestamp()
+
+message.channel.send(botcodeEmbed);
+
+
+
+     //custom commands end here. the welcome code which is disabled and isnt here rn starts here, and so does dm code.
 
       } else if (message.channel.type == "dm") {
         message.author.send(`Go away. I'm not made for use in direct messages.`);
@@ -101,15 +148,4 @@ client.on('message', message => {
       }
     });
 
-client.on('guildMemberAdd', member => {
-    member.guild.channels.get('734174332300820510').send("Welcome to TARDIM.");
-
-});
-
-client.login('NzUzNjUxMDExNjU4NDQ4OTI2.X1pSMw.JP_AvLbKukMUs1n1gzC0R4oFNsg');
-
-client.on("ready", () => {
-    client.user.setActivity("Type >help for help. | Welcome to SMT", { type: "WATCHING", url: "https://www.twitch.tv/cyanideee_" })
-    client.user.setStatus('dnd')
-
-})
+client.login(token);
